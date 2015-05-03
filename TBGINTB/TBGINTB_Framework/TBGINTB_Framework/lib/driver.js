ba6f134bfce77a498c9c0f217b79d0db697377f3
ianeller-romey <ianeller.eller@gmail.com>
2015-05-02 17:02:44 -0700

    GinTub: Adding jQuery
    Began adding jQuery to animate VerbList

--- TBGINTB/TBGINTB_Framework/TBGINTB_Framework/lib/driver.js
+++ TBGINTB/TBGINTB_Framework/TBGINTB_Framework/lib/driver.js
@@ -1,54 +1,54 @@
-﻿(function () {
-    "use strict";
-    
-    window.addEventListener("load", function load(event) { // initialize once the window has loaded
-        window.removeEventListener("load", load, false);
-        init();
-    }, false);
-
-    
-    function init() { // on initialization ...
-        var userInputManager = new UserInputManager();
-        var engine = new Engine();
+﻿
+$(document).ready(function init() { // on initialization ...
+    var userInputManager = new UserInputManager();
+    var engine = new Engine();
         
-        var i = 0;
-        var textParagraphs = document.getElementsByClassName("unParagraph");
-        var verbListElem = document.getElementById("verbList");
-        var interfaceElem = document.getElementById("interface");
+    var i = 0;
+    var textParagraphs = document.getElementsByClassName("unParagraph");
+    var verbListElem = $("#verbList");
+    var interfaceElem = document.getElementById("interface");
 
-        var openVerbListElem = function (event) {
-            verbListElem.style.left = (event.clientX - interfaceElem.offsetLeft) + "px";
-            verbListElem.style.top = (event.clientY - interfaceElem.offsetTop - 20) + "px";
-            var div = document.createElement("div");
-            div.className = "verb";
-            div.innerHTML = "TEST";
-            div.addEventListener("click", closeVerbListElem, true);
-            verbListElem.appendChild(div);
-        };
-        var closeVerbListElem = function (event) {
-            while (verbListElem.firstChild) {
-                verbListElem.removeChild(verbListElem.firstChild);
-            }
-        };
-
-        for (; i < textParagraphs.length; i += 1) {
-            textParagraphs[i].addEventListener("click", closeVerbListElem, true);
-            textParagraphs[i].addEventListener("click", openVerbListElem, true);
-        }
-
-        /*
-        function suppressBackspace(event) { // we don't want the delete key to act like the browser back button
-            event = event || window.event;
-            var target = event.target || event.srcElement;
-            if (event.keyCode == 8 && !/input|textarea/i.test(target.nodeName)) {
-                updateUserInput(event);
-                return false;
-            }
-        };
-        document.onkeydown = suppressBackspace;
-        document.onkeypress = suppressBackspace;
-        */
-
-        
+    var openVerbListElem = function (event) {
+        var div = document.createElement("div");
+        div.className = "verb";
+        div.innerHTML = "TEST";
+        div.addEventListener("click", closeVerbListElem, true);
+        verbListElem.append(div);
+        verbListElem.animate({
+            left: event.clientX - interfaceElem.offsetLeft,
+            top: event.clientY - interfaceElem.offsetTop - 20,
+            width: (4 * 20) + 20,
+            padding: "0 10 0 10"
+        }, function () {
+            verbListElem.animate({
+                height: (1 * 20) + 20,
+                padding: 10
+            })
+        });
     };
-}());
+    var closeVerbListElem = function (event) {
+        while (verbListElem.firstChild) {
+            verbListElem.removeChild(verbListElem.firstChild);
+        }
+    };
+
+    for (; i < textParagraphs.length; i += 1) {
+        textParagraphs[i].addEventListener("click", closeVerbListElem, true);
+        textParagraphs[i].addEventListener("click", openVerbListElem, true);
+    }
+
+    /*
+    function suppressBackspace(event) { // we don't want the delete key to act like the browser back button
+        event = event || window.event;
+        var target = event.target || event.srcElement;
+        if (event.keyCode == 8 && !/input|textarea/i.test(target.nodeName)) {
+            updateUserInput(event);
+            return false;
+        }
+    };
+    document.onkeydown = suppressBackspace;
+    document.onkeypress = suppressBackspace;
+    */
+
+        
+});
